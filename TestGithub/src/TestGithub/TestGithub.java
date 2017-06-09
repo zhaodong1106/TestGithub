216bebebf95ba264dd6838262962f2a567adba9d
zhaodong <1987589041@qq.com>
2017-06-09 22:37:07 +0800

    1th_commit

---
+++ TestGithub/src/TestGithub/TestGithub.java
@@ -0,0 +1,15 @@
+package TestGithub;
+
+public class TestGithub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		say();
+	}
+	public static void say(){
+		System.out.println("dasdasdasd");
+	}
+}
